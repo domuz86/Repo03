759ebd984d960bdad8c6d9174a782ad02698ddcb
domuz86 <muamer.m86@gmail.com>
2022-03-30 14:28:37 +0200

    exo8

---
+++ ex08/src/index.js
@@ -0,0 +1,18 @@
+function main(numOne, numeTwo){
+
+    //Only change code below this line
+    var numOne;
+    var numeTwo;
+
+    var sum = numOne + numeTwo;
+
+    //Only change code above this line
+  
+
+   return sum;
+    
+}
+
+console.log(main(5, 28));
+
+module.exports = main;
